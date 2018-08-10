052330f758b13f635a2ca170f17974005d2290ed
Keith Whitley <keithwhitley@Keiths-MacBook-Pro.local>
2018-08-10 11:50:10 -0400

    fixed deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-S3_CODE_BUCKET=$(aws ssm get-parameter --region us-west-2 --name S3_CODE_BUCKET | jq -r '.Parameter.Value')
-echo $S3_CODE_BUCKET
+LAMBDA_CODE_BUCKET=$(aws ssm get-parameter --region us-west-2 --name LAMBDA_CODE_BUCKET | jq -r '.Parameter.Value')
+echo $LAMBDA_CODE_BUCKET
 
 aws cloudformation package \
 --template-file resources/sam.yaml \
 --region us-west-2
 --output-template-file resources/cf.yaml \
---s3-bucket $S3_CODE_BUCKET --s3-prefix LUAU
+--s3-bucket $LAMBDA_CODE_BUCKET --s3-prefix LUAU
 
 aws cloudformation deploy \
 --region us-west-2 \
