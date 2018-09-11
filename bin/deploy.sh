9796ff1c9270c83c31e3a326deac690f1731493f
Keith Whitley <keithwhitley@Keiths-MacBook-Pro.local>
2018-09-11 10:22:05 -0400

    adding parameters

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -2,14 +2,17 @@
 LAMBDA_CODE_BUCKET=$(aws ssm get-parameter --region us-west-2 --name LAMBDA_CODE_BUCKET | jq -r '.Parameter.Value')
 echo $LAMBDA_CODE_BUCKET
 
+source resources/env.properties
+
 aws cloudformation package \
 --template-file resources/sam.yaml \
---region us-west-2 \
+--region $AWS_REGION \
 --output-template-file resources/cf.yaml \
 --s3-bucket $LAMBDA_CODE_BUCKET --s3-prefix LUAU
 
 aws cloudformation deploy \
---region us-west-2 \
+--region $AWS_REGION \
+--parameter-overrides AWS_REGION=$AWS_REGION SES_EMAIL=$SES_EMAIL ADMIN_EMAIL=$ADMIN_EMAIL\ 
 --template-file resources/cf.yaml \
 --stack-name LUAUTagger \
 --capabilities CAPABILITY_IAM
